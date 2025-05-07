b20e0922eb05abd4d0803916f1512776b0bc5805
Vidal Moreira <moreiravidal22@gmail.com>
2025-05-07 19:08:15 -0300

    Iniciando projeto

---
+++ src/main/java/com/precin/PrecinWebBackApplication.java
@@ -0,0 +1,13 @@
+package com.precin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecinWebBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrecinWebBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/precin/PrecinWebBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.precin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecinWebBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
